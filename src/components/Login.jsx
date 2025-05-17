06ef2c7eabb78195a1ad6268c91acbdbdb7a950a
Helsingbuss <info@helsingbuss.se>
2025-05-17 20:53:09 +0200

    Fix: rättade logotypens sökväg

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { supabase } from '../supabaseClient';
-import logo from '../assets/logo.png';
+import logo from './logo.png';
 import './login.css';
 
 export default function Login() {
