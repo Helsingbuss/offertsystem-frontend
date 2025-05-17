55b99b43d7583e62f5b65e84cd804be09d8e77f1
Helsingbuss <info@helsingbuss.se>
2025-05-17 17:09:22 +0200

    Flyttade Login.jsx till components och tog bort gamla

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
+import Login from "./components/Login";
 import Dashboard from "./pages/Dashboard";
 import BergkvaraDashboard from "./pages/BergkvaraDashboard";
 
